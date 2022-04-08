c0f4c757248b489a0298f94b973b8f0b9095c145
hallen10 <hallen10@ewu.edu>
2022-04-08 14:52:55 -0700

    I added a new main class and added a factory class. Still trying to figure things out.

---
+++ src/Entities/Factory.java
@@ -0,0 +1,7 @@
+package Entites.Entities;
+
+public class Factory {
+
+
+
+}
---
+++ src/FactoryPattern/Main.java
@@ -0,0 +1,11 @@
+package Entites.FactoryPattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+    }
+
+}
