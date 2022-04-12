b7a2290886df943c8a13834a17b645336828ebcb
hallen10 <hallen10@ewu.edu>
2022-04-11 18:42:30 -0700

    Added details about our factories to main.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
+import Mains.simpleMain;
 import SimpleFactory.SimpleFactory;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Hello factories!");
 
-        SimpleFactory factory = new SimpleFactory();
-
-        factory.createMonster("goblin");
+        //Logan's introducing entity. Has messy code.
+        //[name]Main();
+        //Hunter's simple factory. will introduce a single factory that will handle all entity subclasses.
+        simpleMain run = new simpleMain();
+        //Brendan's factories. There will be multiple factories representing genres/types.
+        //factoryMain();
+        //Ryan's abstract factory.
+        //abstractMain();
 
     }
 
