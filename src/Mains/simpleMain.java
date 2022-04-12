4bedc2b22eb3e6307ccd53cfcaa2e86a97e675cd
hallen10 <hallen10@ewu.edu>
2022-04-11 18:43:28 -0700

    Added a simple factory main.

---
+++ src/Mains/simpleMain.java
@@ -0,0 +1,13 @@
+package Mains;
+
+//Made by Hunter Allen
+
+public class simpleMain {
+
+    public simpleMain(){
+        //Just a test
+        System.out.println("Welcome to the simple factory!");
+
+    }
+
+}
