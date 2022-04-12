834eb171b8f8dbad148329bbde2024fe11049f0c
hallen10 <hallen10@ewu.edu>
2022-04-11 20:37:09 -0700

    Added a response to an empty statement.

--- src/SimpleFactory/SimpleFactory.java
+++ src/SimpleFactory/SimpleFactory.java
@@ -3,16 +3,25 @@ package SimpleFactory;
 import Entities.Entity;
 import Entities.Goblin;
 
+//By Hunter
+
 public class SimpleFactory {
 
-    public void createMonster(String monster){
-        Entity entity;
+    public Entity createMonster(String monster){
+        Entity entity = null;
 
-        if (monster.toLowerCase() == "goblin") {
+        //Just to make things simpler
+        String name = monster.toLowerCase();
+
+        if (name.equals("goblin")) {
             entity = new Goblin(10, 10, 10,10,"Timmy");
             System.out.println("You made a goblin!");
-            System.out.println(entity); //Prints entity stats
+            System.out.println(entity+"\n"); //Prints entity stats
+        }else{
+            System.out.println("That Entity does not exist. Try again.\n");
         }
+
+        return entity;
 
     }
 
