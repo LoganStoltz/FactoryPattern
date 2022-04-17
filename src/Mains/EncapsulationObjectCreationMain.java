ca611c0a653314fdc2ff8e169e60e97306063d6c
LoganStoltz <lstoltz@ewu.edu>
2022-04-16 19:29:08 -0700

    My final adjustments for my code

--- src/Mains/EncapsulationObjectCreationMain.java
+++ src/Mains/EncapsulationObjectCreationMain.java
@@ -7,33 +7,32 @@ public class EncapsulationObjectCreationMain
     public static void main(String[] args)
     {
         Entity ent1 = createEntity("goblin");
-        System.out.println(ent1.toString());
+        System.out.println(ent1);
 
         Entity ent2 = createEntity("skeleton");
-        System.out.println(ent2.toString());
+        System.out.println(ent2);
 
         Entity ent3 = createEntity("tarantula");
-        System.out.println(ent3.toString());
+        System.out.println(ent3);
 
         Entity ent4 = createEntity("troll");
-        System.out.println(ent4.toString());
+        System.out.println(ent4);
     }
 
     public static Entity createEntity(String type)
     {
-        Entity entity;
 
         if (type.equals("goblin")){
-            return entity = new Goblin(150,100,50,"Goblin    ");
+            return new Goblin(150,100,50,"Goblin    ");
         }
         else if (type.equals("skeleton")){
-            return entity = new Skeleton(175,175,25,"Skeleton  ");
+            return new Skeleton(175,175,25,"Skeleton  ");
         }
         else if (type.equals("tarantula")){
-            return entity = new Tarantula(125,150,0,"Tarantula ");
+            return new Tarantula(125,150,0,"Tarantula ");
         }
         else if (type.equals("troll")){
-            return entity = new Troll(300,200,100,"Troll     ");
+            return new Troll(300,200,100,"Troll     ");
         }
         else{
             return null;
--- src/Mains/NaiveMain.java
+++ src/Mains/NaiveMain.java
@@ -4,44 +4,46 @@ import NaiveEntities.*;
 
 public class NaiveMain
 {
-/*
- *  This method of object creation is very simple and easy where we can have concrete classes being instantiating
- *  during the runtime of the program with our specified set of arguments
- *
- *  However, if we wanted to expand our code at all this would become a very tedious process depending on the
- *  size complexity of the code.
- *  Also keep in mind this is using very simple Constructors, arguments and methods to make this happen.
- *
- *  An Alternative route would be to also consider using interfaces purely for their ability to work with new
- *  classes that are implementing those interfaces through polymorphism (essentially an object that has the
- *  ability to preform the same action in many ways)
- *
- *  Of course having concrete classes involved in this could lead to similar issues down the road that we were
- *  trying to avoid in the first place.
- *
- *  The answer to this problem are Abstract classes for their flexibility since they cannot be instantiated.
- *  This is a key idea for encapsulating object creation .
- */
+
     public static void main(String[] args)
     {
 
-        Entity entity = new Entity("goblin");
+        Entity goblin1 = new Entity("skeleton");
 
-            if (entity.getName() == "goblin"){
-                entity = new Goblin(150,100,50,"Naive Goblin");
-                System.out.println(entity.toString());
-            }
-            else if (entity.getName() == "skeleton"){
-                entity = new Skeleton(175,175,25,"Naive Skeleton");
-                System.out.println(entity.toString());
-            }
-            else if (entity.getName() == "tarantula"){
-                entity = new Tarantula(125,150,0,"Naive Tarantula");
-                System.out.println(entity.toString());
-            }
-            else if (entity.getName() == "troll"){
-                entity = new Troll(300,200,100,"Naive Troll");
-                System.out.println(entity.toString());
-            }
+        if (goblin1.getName().equals("goblin")){
+            goblin1 = new Goblin(150,100,50,"Naive Goblin   ");
+            System.out.println(goblin1);
+        }
+        else if (goblin1.getName().equals("skeleton")){
+            goblin1 = new Skeleton(175,175,25,"Naive Skeleton ");
+            System.out.println(goblin1);
+        }
+        else if (goblin1.getName().equals("tarantula")){
+            goblin1 = new Tarantula(125,150,0,"Naive Tarantula");
+            System.out.println(goblin1);
+        }
+        else if (goblin1.getName().equals("troll")){
+            goblin1 = new Troll(300,200,100,"Naive Troll    ");
+            System.out.println(goblin1);
+        }
+
+
+        /*
+        Entity goblin2 = new Entity("goblin");
+        goblin2 = new Goblin(150,100,50,"Naive Goblin   ");
+        System.out.println(goblin2.toString());
+
+        Entity skeleton = new Entity("skeleton");
+        skeleton = new Skeleton(175,175,25,"Naive Skeleton ");
+        System.out.println(skeleton.toString());
+
+        Entity tarantula = new Entity("tarantula");
+        tarantula = new Tarantula(125,150,0,"Naive Tarantula");
+        System.out.println(tarantula.toString());
+
+        Entity troll = new Entity("troll");
+        troll = new Troll(300,200,100,"Naive Troll    ");
+        System.out.println(troll.toString());
+        */
     }
 }
